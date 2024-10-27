520a31688298afaf2ba3ac4abd9f2aaa6a705900
Mohammad Karam <mkaram84@hotmail.com>
2024-10-27 06:24:06 +0300

    handle arguments

--- TaskTracker.App/Program.cs
+++ TaskTracker.App/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace TaskTracker.App;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using TaskTracker.App.Business;
+
+namespace TaskTracker.App;
 
 internal class Program
 {
@@ -10,11 +14,58 @@ internal class Program
             return;
         }
 
-        foreach (var arg in args)
-        {
-            Console.WriteLine($"Argument: {arg}");
-        }
+        var taskStore = new TaskStore(){FilePath = Path.Combine(AppContext.BaseDirectory, "Data.json")};
+        var taskTracker = new Business.TaskTracker(taskStore);
 
-        Console.WriteLine("Hello, World!");
+        if(args[0] == typeof(TaskCommand).GetField(TaskCommand.Add.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var description = args[1];
+            var result = taskTracker.AddTask(description);
+        }
+        else if(args[0] == typeof(TaskCommand).GetField(TaskCommand.Update.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var id = Convert.ToInt32(args[1]);
+            var description = args[2];
+            var result = taskTracker.UpdateTaskDescription(id, description);
+        }
+        else if(args[0] == typeof(TaskCommand).GetField(TaskCommand.Delete.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var id = Convert.ToInt32(args[1]);
+            var result = taskTracker.DeleteTask(id);
+        }
+        else if(args[0] == typeof(TaskCommand).GetField(TaskCommand.MarkInProgress.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var id = Convert.ToInt32(args[1]);
+            var result = taskTracker.UpdateTaskStatus(id, Business.TaskStatus.InProgress);
+        }
+        else if(args[0] == typeof(TaskCommand).GetField(TaskCommand.MarkDone.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var id = Convert.ToInt32(args[1]);
+            var result = taskTracker.UpdateTaskStatus(id, Business.TaskStatus.Done);
+        }
+        else if(args[0] == typeof(TaskCommand).GetField(TaskCommand.List.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+        {
+            var tasks = new List<Business.Task>();
+            if(string.IsNullOrEmpty(args[1])) 
+            {
+                tasks = taskTracker.ListAllTasks();
+            }
+            else
+            {
+                Business.TaskStatus taskStatus;
+                if(args[1] == typeof(TaskSubCommandForList).GetField(TaskSubCommandForList.Done.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+                    taskStatus = Business.TaskStatus.Done;
+                else if(args[1] == typeof(TaskSubCommandForList).GetField(TaskSubCommandForList.InProgress.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+                    taskStatus = Business.TaskStatus.InProgress;
+                else if(args[1] == typeof(TaskSubCommandForList).GetField(TaskSubCommandForList.Todo.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name)
+                    taskStatus = Business.TaskStatus.Todo;
+                else
+                    taskStatus = Business.TaskStatus.Todo;
+
+                tasks = taskTracker.ListTasksByStatus(taskStatus);
+            
+            }
+        }
+        
     }
 }
